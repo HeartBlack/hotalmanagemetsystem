141976bd2912e5d6325520e7222e73bfa31a5b64
Chetan Gartoula <63311697+HeartBlack@users.noreply.github.com>
2022-07-01 14:11:29 +0545

    change debug

--- HMS/settings.py
+++ HMS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-unbee8ai38lh+uqk+3l1a1k1b(=^wxc1kh!xw)!(zmuq90=m!2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
